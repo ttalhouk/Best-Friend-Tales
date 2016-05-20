d673ba5861204dbbba154389d4ee66622f5d0750
Talal Talhouk <email": null>
2016-05-19 18:11:25 -0700

    update migrations to remove email null validation

--- db/migrate/20160519145715_update_users.rb
+++ db/migrate/20160519145715_update_users.rb
@@ -1,6 +1,6 @@
 class UpdateUsers < ActiveRecord::Migration
   def change
     add_column :users, :zip, :string, null: false, default: "94112"
-    add_column :users, :email, :string, null: false
+    add_column :users, :email, :string
   end
 end

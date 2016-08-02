2d8305c73e5cc00ada6f7f3f4338051c48a9bfea
Talal Talhouk <talal@sbcglobal.net>
2016-08-02 10:24:05 -0700

    update routes

--- rails_version/config/routes.rb
+++ rails_version/config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
 	root "pages#home"
   devise_for :users
+
+	resources :users, except: :destroy do
+		resources :posts, except: :destroy do
+			resources :comments, only: [:new, :create]
+		end
+		resources :pets, execpt: [:edit, :update] do
+			get "/mailers/new" => "mailer#new"
+		end
+	end
+	get "/adoptions" => "adoptions#random"
+	get "/adopition/zip" => "adoptions#local"
+
+
 end

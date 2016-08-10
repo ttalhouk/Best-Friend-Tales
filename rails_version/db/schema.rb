b1fd0fe253fcb399756652b386808dbf2abee6c9
Talal Talhouk <talal@sbcglobal.net>
2016-08-10 13:57:37 -0700

    update database schema

--- rails_version/db/schema.rb
+++ rails_version/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160802134718) do
+ActiveRecord::Schema.define(version: 20160802165611) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "body"
@@ -55,6 +55,15 @@ ActiveRecord::Schema.define(version: 20160802134718) do
     t.integer  "user_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.string   "username"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
